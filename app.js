6d050074eba5280453c956c625994d720d288221
darsh <patelda5@msu.edu>
2024-12-17 12:07:48 -0500

    Fix file paths and project structure for deployment

--- app.js
+++ app.js
@@ -3,40 +3,31 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serving static files from 'public' directory
-app.use(express.static('public'));
+// Serve static files (CSS, JS, Images) from the root directory
+app.use(express.static(__dirname));
 
-// Route for homepage
+// Routes for your pages
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-// Route for About page
 app.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'about.html'));
+    res.sendFile(path.join(__dirname, 'about.html'));
 });
 
-// Route for Experience page
 app.get('/experience', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'experience.html'));
+    res.sendFile(path.join(__dirname, 'experience.html'));
 });
 
-// Route for Projects page
 app.get('/projects', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'projects.html'));
+    res.sendFile(path.join(__dirname, 'projects.html'));
 });
 
-// Route for Contact page
 app.get('/contact', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+    res.sendFile(path.join(__dirname, 'contact.html'));
 });
 
-// Handle 404 - Not found
-app.use((req, res) => {
-    res.status(404).send("Sorry, we can't find that!");
-});
-
-// Starting the server
+// Start the server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
